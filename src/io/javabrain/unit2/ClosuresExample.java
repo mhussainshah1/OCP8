3c364cdab869d4ba88cc32c4bff97c67db100b60
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-09-14 19:57:37 +0300

    Chapter 1

--- src/io/javabrain/unit2/ClosuresExample.java
+++ src/io/javabrain/unit2/ClosuresExample.java
@@ -6,16 +6,18 @@ public class ClosuresExample {
 
     public static void main(String[] args) {
         int a = 10;
-        int b = 20;
-        //previously
+        int b = 20;//effectively final
+        //previously in java 7
+        //final int b =20;
+        
         doProcess(a, new IntConsumer() {
             @Override
             public void accept(int i) {
-                //b =40;//Error:local variable referenced inside inner class must be effectively final or final
+                b =40;//Error:local variable referenced inside inner class must be effectively final or final
                 System.out.println(i + b);
             }
         });
-        //now in java 8
+        //Alternatively, now in java 8
         doProcess(a, i -> System.out.println(i + b));
     }
 
